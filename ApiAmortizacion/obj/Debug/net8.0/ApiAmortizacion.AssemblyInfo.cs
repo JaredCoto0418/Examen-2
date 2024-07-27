331405af307f6753ce98975adade88cdb1a3837f
JaredCoto0418 <coto@gmail.com>
2024-07-26 22:48:49 -0600

    260724

--- ApiAmortizacion/obj/Debug/net8.0/ApiAmortizacion.AssemblyInfo.cs
+++ ApiAmortizacion/obj/Debug/net8.0/ApiAmortizacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiAmortizacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d3e5851d69f9ce1343057bcc343c09cba67961")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiAmortizacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiAmortizacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
